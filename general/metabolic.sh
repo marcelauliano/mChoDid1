6eea48553107b4ec813017614c3c7993c497c739
Marcela Uliano-Silva <marcela.uliano@gmail.com>
2022-07-22 11:25:38 +0100

    Create metabolic.sh

---
+++ general/metabolic.sh
@@ -0,0 +1,14 @@
+
+
+longest_iso_CD= "/lustre/scratch123/tol/teams/tola/users/mu2/from_scratch116/mChoDid1-paper/orthofinder-Longest/mchodid1-GCF_015220235.1_aa.longestn1.fa"
+proteins_name_descriptions_fasta="/lustre/scratch123/tol/teams/tola/users/mu2/from_scratch116/mChoDid1-paper/orthofinder_names/GCF_015220235.1_mChoDid1.pri_protein.name.faa"
+
+#protein ids with tab separated id
+proteins_name_descriptions_ids="/lustre/scratch123/tol/teams/tola/users/mu2/from_scratch116/mChoDid1-paper/orthofinder-Longest/mChoDid1.prot.tab.ids"
+
+
+#giving a gene name
+
+grep "lactate dehydrogenase" $proteins_name_descriptions_ids
+
+
